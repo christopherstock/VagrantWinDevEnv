e6a1f84501d472581248357260d30ca23162c2e2
Christopher Stock <christopher.stock@mayflower.de>
2020-10-12 13:38:21 +0200

    Unify provision scripts.

--- scripts/provision_deployment.ps1
+++ scripts/provision_deployment.ps1
@@ -1,2 +1,6 @@
 
     echo "scripts/provision_deployment.ps1 - Run Provisioning for the Deployment machine"
+
+    # sync Chocolatey repository and install package 'AutoIt'
+    choco sync
+    choco install -y autoit
--- scripts/provision_development.ps1
+++ scripts/provision_development.ps1
@@ -1,2 +1,6 @@
 
     echo "scripts/provision_development.ps1 - Run Provisioning for the Development machine"
+
+    # sync Chocolatey repository and install package 'Sysinternals'
+    choco sync
+    choco install -y sysinternals
