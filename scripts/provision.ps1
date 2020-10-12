ca4f9a4fed0e014cd39558d7477a5656a51e1611
Christopher Stock <christopher.stock@mayflower.de>
2020-10-12 13:28:09 +0200

    Complete provision script for both machines.

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -1,19 +1,19 @@
 
     echo "scripts/provision.ps1 - Run Provisioning for both machines"
 
-    try {
-      echo " Trying to find Chocolatey on the Windows machine"
-      choco -v
-      echo " Chocolatey has successfully been detected"
+    try
+    {
+        # check if Chocolatey is installed
+        echo " Trying to find Chocolatey on the Windows machine"
+        choco -v
+        echo " Chocolatey has successfully been detected"
     }
     Catch
     {
-      Set-ExecutionPolicy Bypass -Scope Process -Force
+        # allow PowerShell script execution
+        Set-ExecutionPolicy Bypass -Scope Process -Force
 
-      # install Windows package manager (Chocolatey)
-
-      # TODO required?
-      [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-
-      iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        # install Chocolatey from the website
+        echo "Installing Chocolatey right away"
+        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     }
