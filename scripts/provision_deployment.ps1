a65e6f6fe9c9195c23ab74d6260129b883410f4f
Christopher Stock <christopher.stock@mayflower.de>
2020-10-12 15:19:29 +0200

    Complete development script.

--- scripts/provision_deployment.ps1
+++ scripts/provision_deployment.ps1
@@ -2,5 +2,6 @@
     echo "scripts/provision_deployment.ps1 - Run Provisioning for the Deployment machine"
 
     # sync Chocolatey repository and install package 'AutoIt'
-    choco sync
     choco install -y autoit
+
+    echo " Completed provisioning the deployment machine"
--- scripts/provision_development.ps1
+++ scripts/provision_development.ps1
@@ -1,6 +1,15 @@
 
     echo "scripts/provision_development.ps1 - Run Provisioning for the Development machine"
 
-    # sync Chocolatey repository and install package 'Sysinternals'
-    choco sync
+    # define major paths in variables
+    $vagrantPath = "C:/vagrant"
+    $userPath = "C:/Users/vagrant"
+
+    # sync Chocolatey repository and install all required packages 'Sysinternals'
     choco install -y sysinternals
+
+    # create a directory listing of the vagrant synced dir in a readme.txt on the windows desktop
+    & cd "$vagrantPath"
+    & dir > "$userPath/Desktop/readme.txt"
+
+    echo " Completed provisioning the development machine"
